85b4b4619f01060bee50a6300706d65fc07bb632
Arjun Jumbade <arjunjumbade1@gmail.com>
2026-08-14 15:23:26 +0530

    give routing

--- myproject/src/App.jsx
+++ myproject/src/App.jsx
@@ -1,24 +1,13 @@
 import React from "react";
-import Contact from './Components/Contact'
-import Navbar from "./components/Navbar";
-import About from './Components/About'
-import Event from "./Components/Event";
-import Applications from "./Components/Applications";
-import CraftVendors from "./Components/CraftVendors";
-import FoodVendors from "./Components/FoodVendors";
+import Navbar from "./Components/Navbar";
+import AppRoutes from "./Routes/AppRoutes";
 import Footer from './Components/Footer'
 
 const App = () => {
     return (
-
         <div>
             <Navbar />
-            <Contact />
-            <Event />
-            <About />
-            <Applications />
-            <CraftVendors />
-            <FoodVendors />
+            <AppRoutes />
             <Footer />
         </div>
     );
