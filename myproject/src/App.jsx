883675dc672d413e5632a87016acf25e8be2583b
pankaj <pankajlipane02@gmail.com>
2026-08-14 12:56:42 +0530

    make the craftvenders

--- myproject/src/App.jsx
+++ myproject/src/App.jsx
@@ -1,19 +1,26 @@
 import React from "react";
-import Contact from './Components/Contact'
-import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Contact from "./Components/Contact";
 import Applications from "./Components/Applications";
 import CraftVendors from "./Components/CraftVendors";
 import FoodVendors from "./Components/FoodVendors";
 
 const App = () => {
   return (
-
     <div>
       <Navbar />
-      <Contact />
-      <Applications />
-      <CraftVendors />
-      <FoodVendors />
+      <Routes>
+        <Route path="/" element={<Contact />} />
+        <Route path="/about" element={<Contact />} />
+        <Route path="/vendors" element={<Contact />} />
+        <Route path="/vendors/craft" element={<CraftVendors />} />
+        <Route path="/vendors/food" element={<FoodVendors />} />
+        <Route path="/vendors/applications" element={<Applications />} />
+        <Route path="/events" element={<Contact />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/tickets" element={<Contact />} />
+      </Routes>
     </div>
   );
 };
